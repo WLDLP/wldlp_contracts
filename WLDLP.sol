c208837f166c73c041f777af0cfa7aed7d2c662d
wldlpdev <130398667+wldlpdev@users.noreply.github.com>
2023-04-11 00:28:26 +0530

    Forking Openzeippelin contracts to local repo
    Forking openzeippelin contracts to local repo if required in some cases.

---
+++ WLDLP.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "wldlp_contracts/token/ERC20/ERC20.sol";
+import "wldlp_contracts/access/AccessControl.sol";
+import "wldlp_contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+
+/*
+//can also use openzeppelin contracts directly
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+*/
+
+contract WLDLP is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant RELAY_ROLE = keccak256("RELAY_ROLE");
+
+    string name1 = "World Largest Decentralised Liquidity Protocol";
+    string symbol1 = "WLDLP";
+
+    constructor(address _minter) ERC20(name1, symbol1) {
+        _mint(_minter, 1000000005 * 10 ** decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+    
+
+//Function  to mint more WLDLP as required only by authorised addresses
+    function relayMint(
+        address to,
+        uint256 value
+    ) public onlyRole(RELAY_ROLE) returns (bool) {
+        _mint(to, value);
+        return true;
+    }
+}
